2d876617f323c1523b69c25d387aa49abdb8f279
Mike <vip.bmv2002@gmail.com>
2026-05-17 19:29:08 +0300

    добавил мидлу для логирования (#5)
    * добавил мидлу для логирования

--- internal/server/hmiddlewares/logging.go
+++ internal/server/hmiddlewares/logging.go
@@ -2,11 +2,36 @@ package hmiddlewares
 
 import (
 	"net/http"
+
+	"github.com/rs/zerolog/log"
 )
 
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
 func LoggingMiddleware(next http.Handler) http.Handler {
+
+	// вывод в json
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		next.ServeHTTP(w, r)
-		// TODO  реализовать логирование информации о запросах через zerolog
+		wrapped := &responseWriter{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+
+		next.ServeHTTP(wrapped, r)
+
+		log.Logger.Info().
+			Str("method", r.Method).
+			Str("path", r.URL.Path).
+			Int("status", wrapped.statusCode).
+			Msg("HTTP Request processed")
+
 	})
 }
