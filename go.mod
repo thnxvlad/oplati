7a173667ee0b07ba59224585767f648518e44566
mikee <vip.bmv2002@gmail.com>
2026-05-18 20:19:03 +0300

    написал логику в пакете бд

--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module github.com/thnxvlad/oplati
 go 1.25.0
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.3.1
 	github.com/google/uuid v1.6.0
 	github.com/rs/zerolog v1.35.1
+	golang.org/x/crypto v0.51.0
 )
 
 require (
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/sys v0.44.0 // indirect
 )
--- internal/storages/inmemory/auth/storage.go
+++ internal/storages/inmemory/auth/storage.go
@@ -1,3 +1,81 @@
 package auth
 
+import (
+	"errors"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
 // ToDo: реализовать всё
+var (
+jwtSecret = os.Getenv("JWT_SECRET")
+loginData = make(map[string]string)
+accountData = make(map[string]string)
+authMu sync.RWMutex
+) 
+
+type userClaims struct {
+	UserID string `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
+func generateToken(userID string) (string, error) {
+	now := time.Now()
+	claims := &userClaims{
+		UserID: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(now.Add(24*time.Hour)),
+			IssuedAt: jwt.NewNumericDate(now),
+			NotBefore: jwt.NewNumericDate(now),
+			Issuer: "oplati-api",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	return token.SignedString([]byte(jwtSecret))
+}
+
+func validateLogin(login, password string) bool {
+	hashedPassword, ok := loginData[login]
+	if !ok {
+		return false
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
+}
+
+func Login(login, password string) (string, error) {
+	authMu.Lock()
+	defer authMu.Unlock()
+
+	if !validateLogin(login, password) {
+		return "",errors.New("invalid login or password")
+	}
+
+	accountId, ok := accountData[login]
+	if !ok {
+		return "",errors.New("account not found")
+	}
+
+	return generateToken(accountId)
+}
+
+func GetAccountIdFromToken(token string) (string, error) {
+	claims, err := jwt.ParseWithClaims(token, &userClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(jwtSecret), nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	parsedClaims, ok := claims.Claims.(*userClaims)
+	if !ok {
+		return "", errors.New("invalid claims type")
+	}
+
+	return parsedClaims.UserID, nil
+}
