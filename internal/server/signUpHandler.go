dd65411912aef27d3a79b02536219ef59a383133
mikee <vip.bmv2002@gmail.com>
2026-05-21 21:07:13 +0300

    1) исправил нэйминг 2) логгер 3) оставил только один метод signUp 4) иссправил ошибку : badrequest -> internal error

--- internal/server/signUpHandler.go
+++ internal/server/signUpHandler.go
@@ -4,59 +4,42 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
-type RegisterRequest struct {
-	Name     string `json:"name"`
+type SignUpRequest struct {
 	Login    string `json:"login"`
 	Password string `json:"password"`
 }
 
-type RegisterResponse struct {
+type SignUpResponse struct {
 	Token string `json:"token"`
 }
 
-func (s *PrivateServer) registerHandler(w http.ResponseWriter, r *http.Request) {
-	logger := zerolog.Ctx(r.Context())
+func (s *PrivateServer) signUpHandler(w http.ResponseWriter, r *http.Request) {
+	logger := log.Logger
 
-	req := RegisterRequest{}
+	req := SignUpRequest{}
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
-		logger.Warn().Err(err).Msg("failed to decode request")
 		http.Error(w, "invalid request body", http.StatusBadRequest)
 		return
 	}
 
 	if req.Login == "" || req.Password == "" {
-		logger.Warn().Msg("empty login or password")
 		http.Error(w, "login and password are required", http.StatusBadRequest)
 		return
 	}
 
-	user, err := s.oplatiService.CreateUser(r.Context(), req.Name)
-	if err != nil {
-		logger.Error().Err(err).Msg("failed to create user in storage")
-		http.Error(w, "could not create user", http.StatusInternalServerError)
-		return
-	}
-
-	err = s.authService.SignUp(req.Login, req.Password, user.Id.String())
+	token, err := s.authService.SignUp(req.Login, req.Password)
 	if err != nil {
 		logger.Error().Err(err).Str("login", req.Login).Msg("failed to sign up in auth service")
-		http.Error(w, "registration failed", http.StatusBadRequest)
-		return
-	}
-
-	token, err := s.authService.SignIn(req.Login, req.Password)
-	if err != nil {
-		logger.Error().Err(err).Msg("failed to login after registration")
-		http.Error(w, "auth failed", http.StatusInternalServerError)
+		http.Error(w, "registration failed", http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(RegisterResponse{Token: token}); err != nil {
+	if err := json.NewEncoder(w).Encode(SignUpResponse{Token: token}); err != nil {
 		logger.Error().Err(err).Msg("failed to encode response")
 	}
 }
