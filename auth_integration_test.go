cd30cd73237910bbeb06e3c1cdb98746a43de721
mikee <vip.bmv2002@gmail.com>
2026-06-04 23:51:42 +0300

    протестирован метод регистрации для двух тестов

---
+++ auth_integration_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/thnxvlad/oplati/internal/service/auth"
+	pgStorage "github.com/thnxvlad/oplati/internal/storages/postgres/auth"
+)
+
+var databaseURL string
+var testPool *pgxpool.Pool
+
+func TestMain(m *testing.M) {
+	var err error
+	ctx := context.Background()
+	databaseURL = "postgres://oplati:oplati@localhost:5432/oplati?sslmode=disable"
+	testPool, err = pgxpool.New(ctx, databaseURL)
+	if err != nil {
+		panic(err)
+	}
+
+	code := m.Run()
+
+	os.Exit(code)
+}
+
+func setupTest(t *testing.T) {
+	t.Helper()
+	_, err := testPool.Exec(context.Background(), "TRUNCATE TABLE accounts, users CASCADE")
+	if err != nil {
+		t.Fatalf("failed to truncate tables: %v", err)
+	}
+}
+
+func TestSignUp_Integration(t *testing.T) {
+	setupTest(t)
+
+	storage := pgStorage.New(testPool)
+	service := auth.New(storage, &mockOplati{})
+
+	ctx := context.Background()
+	login := "mishanya_test"
+	password := "mishanya's pswrd"
+
+	token, err := service.SignUp(ctx, login, password)
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	if token == "" {
+		t.Error("token is empty")
+	}
+
+	_, err = service.SignUp(ctx, login, password)
+	if err == nil {
+		t.Error("expected error cause it's a duplicate")
+	}
+}
---
+++ auth_test.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/thnxvlad/oplati/internal/service/auth"
+)
+
+type mockDB struct {
+	users map[string]struct {
+		id   string
+		hash string
+	}
+}
+
+func (m *mockDB) GetUserByLogin(ctx context.Context, login string) (string, string, error) {
+	u, ok := m.users[login]
+	if !ok {
+		return "", "", errors.New("not found")
+	}
+	return u.id, u.hash, nil
+}
+
+func (m *mockDB) SignUp(ctx context.Context, login, password, userID string) error {
+	m.users[login] = struct {
+		id   string
+		hash string
+	}{id: userID, hash: password}
+	return nil
+}
+
+type mockOplati struct {
+	calledWithID uuid.UUID
+}
+
+func (m *mockOplati) CreateUser(ctx context.Context, userID uuid.UUID) error {
+	m.calledWithID = userID // Сохраняем ID, чтобы проверить его в тесте
+	return nil
+}
+
+func TestSignUp(t *testing.T) {
+	os.Setenv("JWT_SECRET", "test_sercet")
+
+	tests := []struct {
+		name     string
+		login    string
+		password string
+		wantErr  bool
+	}{
+		{
+			name:     "mishanya_test",
+			login:    "mik33",
+			password: "mostpowerfulpswrd",
+			wantErr:  false,
+		},
+		{
+			name:     "andre_test",
+			login:    "andr",
+			password: "mostp649884648owerfulpswrd",
+			wantErr:  false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			db := &mockDB{users: make(map[string]struct {
+				id   string
+				hash string
+			})}
+			oplati := &mockOplati{}
+			service := auth.New(db, oplati)
+
+			token, err := service.SignUp(context.Background(), tt.login, tt.password)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SignUp() error = %v, wanted error = %v", err, tt.wantErr)
+				return
+			}
+
+			if token == "" {
+				t.Error("expected token, get empty string")
+			}
+
+			userInDB, ok := db.users[tt.login]
+			if !ok {
+				t.Error("user is not saved in db")
+			}
+
+			if userInDB.hash == tt.password {
+				t.Error("password is not hashed")
+			}
+
+			if oplati.calledWithID == uuid.Nil {
+				t.Error("OplatiService.CreateUser was not called")
+			}
+		})
+	}
+}
